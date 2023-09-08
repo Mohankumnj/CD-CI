54e35cc339b95bc0a60c814d96bed7ae2420890d
Mohankumnj <117709754+Mohankumnj@users.noreply.github.com>
2023-09-08 15:35:29 +0530

    Update CreateFile.ps1

--- scripts/CreateFile.ps1
+++ scripts/CreateFile.ps1
@@ -1,2 +1,3 @@
 # PowerShell script (CreateFile.ps1)
 New-Item -Path "C:\Users\SSS20220222\OneDrive\Documents\Manoj.txt" -ItemType "file" -Force
+New-Item -Path "C:\Users\SSS20220222\OneDrive\Documents\mere.txt" -ItemType "file" -Force
